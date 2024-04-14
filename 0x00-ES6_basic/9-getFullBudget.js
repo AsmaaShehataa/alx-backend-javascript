c332f267e3de82f992893374544d647490f0d6d9
AsmaaShehataa <asmaa_shihata@yahoo.com>
2024-04-15 01:56:58 +0200

    code fix

---
+++ 0x00-ES6_basic/9-getFullBudget.js
@@ -0,0 +1,17 @@
+// eslint-disable-next-line import/no-named-as-default, import/no-named-as-default-member
+import getBudgetObject from './7-getBudgetObject';
+
+export default function getFullBudgetObject(income, gdp, capita) {
+  const budget = getBudgetObject(income, gdp, capita);
+  const fullBudget = {
+    ...budget,
+    getIncomeInDollars(income) {
+      return `$${income}`;
+    },
+    getIncomeInEuros(income) {
+      return `${income} euros`;
+    },
+  };
+
+  return fullBudget;
+}
