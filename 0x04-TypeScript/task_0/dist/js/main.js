b4aa52e4b272a8fa476af1a6c769bcff19ff599e
AsmaaShehataa <asmaa_shihata@yahoo.com>
2024-04-18 00:05:19 +0200

    Add TypeScript configuration files and ESLint configuration files

---
+++ 0x04-TypeScript/task_0/dist/js/main.js
@@ -0,0 +1,14 @@
+var student1 = {
+    firstName: 'John',
+    lastName: 'Doe',
+    age: 21,
+    location: 'USA'
+};
+var student2 = {
+    firstName: 'George',
+    lastName: 'Daniel',
+    age: 22,
+    location: 'UK'
+};
+console.log(student1, student2);
+//# sourceMappingURL=main.js.map
