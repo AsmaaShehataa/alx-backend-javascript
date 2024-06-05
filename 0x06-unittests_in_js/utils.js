92bd0e1372787f4e4f0477dbdb8edd30cd63ce3f
AsmaaShehataa <asmaa_shihata@yahoo.com>
2024-06-05 16:47:50 +0300

    chore: Update author name in package.json

--- 0x06-unittests_in_js/utils.js
+++ 0x06-unittests_in_js/utils.js
@@ -1,17 +1,19 @@
 const Utils = {
-  calculateNumber (type, a, b) {
+  calculateNumber: function calculateNumber(type, a, b) {
     if (type === 'SUM') {
       return Math.round(a) + Math.round(b);
-    } else if (type === 'SUBTRACT') {
+    }
+    if (type === 'SUBTRACT') {
       return Math.round(a) - Math.round(b);
-    } else if (type === 'DIVIDE') {
+    }
+    if (type === 'DIVIDE') {
       if (Math.round(b) === 0) {
         return 'Error';
       }
       return Math.round(a) / Math.round(b);
-    } else {
-      throw new Error('Unknown type: ' + type);
     }
-  }
+    return 0;
+  },
 };
+
 module.exports = Utils;
