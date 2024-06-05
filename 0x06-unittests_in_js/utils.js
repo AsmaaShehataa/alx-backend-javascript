50a0c188a310374bfbcb40a4f07e06127d3ba613
AsmaaShehataa <asmaa_shihata@yahoo.com>
2024-06-05 18:18:17 +0300

    refactor: Refactor calculateNumber function in utils.js for better readability and maintainability

--- 0x06-unittests_in_js/utils.js
+++ 0x06-unittests_in_js/utils.js
@@ -1,19 +1,17 @@
 const Utils = {
-  calculateNumber: function calculateNumber(type, a, b) {
+  calculateNumber (type, a, b) {
     if (type === 'SUM') {
       return Math.round(a) + Math.round(b);
-    }
-    if (type === 'SUBTRACT') {
+    } else if (type === 'SUBTRACT') {
       return Math.round(a) - Math.round(b);
-    }
-    if (type === 'DIVIDE') {
+    } else if (type === 'DIVIDE') {
       if (Math.round(b) === 0) {
         return 'Error';
       }
       return Math.round(a) / Math.round(b);
+    } else {
+      throw new Error('Unknown type: ' + type);
     }
-    return 0;
-  },
+  }
 };
-
 module.exports = Utils;
